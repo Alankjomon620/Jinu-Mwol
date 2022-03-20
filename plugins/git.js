125d2d6e90310546cbd4b9a9e59fedcfd614cd0f
ɪᴛᴢ ᴍᴇ ᴊɪɴᴜ <101920561+Jinu-Xr@users.noreply.github.com>
2022-03-20 13:48:54 +0530

    Nᴇᴡ ᴀᴅᴅ💫😌

---
+++ plugins/git.js
@@ -0,0 +1,40 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+const fs = require('fs');
+const got = require('got');
+const config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: 'command help menu'}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    r_text[0] = Config.LG_LOGO
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.jpg, contextInfo: {mentionedJid: message.mention}, caption: `
+   ` + config.LOGO_NAME + `
+
+*Oᴡɴᴇʀ Nᴜᴍʙᴇʀ* :https://wa.me/` + config.PHONE + `*            
+
+
+*ɪɴsᴛᴀ ʟɪɴᴋ* :https://wa.me/` + config.INSTA + `*  
+
+
+*Wʜᴀᴛsᴀᴘᴘ Gʀᴏᴜᴘ* :` + config.GROUP + `*
+
+
+*Gɪᴛ Lɪɴᴋs* : https://github.com/Afx-Abu/Luzi-Mwol*
+
+
+*ᴛʜɪs ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ ʙʏ ` + config.OA_NAME + `*
+
+`}) 
+
+}));
